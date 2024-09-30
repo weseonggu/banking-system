063cead3fbb4793d5d27c6b7fcd41bfea53f0e6d
DESKTOP-N8PDVAS\WeSeongGu <fkskdldh@naver.com>
2024-09-30 14:19:28 +0900

    init authservice

--- com.banking.auth/src/main/resources/schema.sql
+++
@@ -1,3 +0,0 @@
-CREATE UNIQUE INDEX unique_username_idx ON p_customer (username) WHERE is_delete = false;
-CREATE UNIQUE INDEX unique_email_idx ON p_customer (email) WHERE is_delete = false;
-CREATE UNIQUE INDEX unique_phone_number_idx ON p_customer (phone_number) WHERE is_delete = false;
