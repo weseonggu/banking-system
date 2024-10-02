0b01c80e8fba47ed57fe45207f85eccfb4160d8b
beomsu1 <beumsu1221@gmail.com>
2024-10-02 15:52:41 +0900

    feat : PersonalHistory KafkaListener 추가

---
+++ com.banking.personal/src/main/java/com/msa/banking/personal/application/dto/event/AccountCompletedEventDto.java
@@ -0,0 +1,18 @@
+package com.msa.banking.personal.application.dto.event;
+
+import com.msa.banking.personal.domain.enums.PersonalHistoryType;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+public class AccountCompletedEventDto {
+
+    private UUID userId;
+    private BigDecimal amount;
+    private PersonalHistoryType type;
+    private String description;
+    private LocalDateTime transactionDate;
+}
