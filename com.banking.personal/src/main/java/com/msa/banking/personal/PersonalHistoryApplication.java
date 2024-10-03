e0ebdddfaa83e3e483e96e9d04142a7cadaef8fb
beomsu1 <beumsu1221@gmail.com>
2024-10-03 18:32:25 +0900

    config: 설정 추가

--- com.banking.personal/src/main/java/com/msa/banking/personal/PersonalHistoryApplication.java
+++ com.banking.personal/src/main/java/com/msa/banking/personal/PersonalHistoryApplication.java
@@ -3,12 +3,14 @@ package com.msa.banking.personal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(scanBasePackages = {
         "com.msa.banking.personal",
         "com.msa.banking.commonbean"
 })
 @EnableFeignClients
+@EnableJpaAuditing
 public class PersonalHistoryApplication {
 
     public static void main(String[] args) {
