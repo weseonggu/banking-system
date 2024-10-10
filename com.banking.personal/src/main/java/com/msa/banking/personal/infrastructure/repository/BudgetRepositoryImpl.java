b610178971e3cced48a5a0ad4945d2df71845f4d
beomsu1 <beumsu1221@gmail.com>
2024-10-10 12:17:28 +0900

    feat: findAllByUserIdAndPeriod 메서드 추가

--- com.banking.personal/src/main/java/com/msa/banking/personal/infrastructure/repository/BudgetRepositoryImpl.java
+++ com.banking.personal/src/main/java/com/msa/banking/personal/infrastructure/repository/BudgetRepositoryImpl.java
@@ -2,9 +2,11 @@ package com.msa.banking.personal.infrastructure.repository;
 
 import com.msa.banking.personal.domain.model.Budget;
 import com.msa.banking.personal.domain.repository.BudgetRepository;
+import feign.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -16,5 +18,7 @@ public interface BudgetRepositoryImpl extends JpaRepository<Budget, UUID>, Budge
     Page<Budget> findAllByIsDeleteFalse(Pageable pageable);
 
     @Override
-    List<Budget> findAllByUserIdAndPeriod(UUID userId, LocalDateTime transactionDate);
+    @Query("SELECT b FROM Budget b WHERE b.userId = :userId AND b.startDate <= :transactionDate AND b.endDate >= :transactionDate AND b.isDelete = false")
+    List<Budget> findAllByUserIdAndPeriod(@Param("userId") UUID userId,
+                                          @Param("transactionDate") LocalDateTime transactionDate);
 }
--- com.banking.personal/src/main/java/com/msa/banking/personal/infrastructure/repository/PersonalHistoryJpaRepository.java
+++ com.banking.personal/src/main/java/com/msa/banking/personal/infrastructure/repository/PersonalHistoryJpaRepository.java
@@ -2,11 +2,16 @@ package com.msa.banking.personal.infrastructure.repository;
 
 import com.msa.banking.personal.domain.model.PersonalHistory;
 import com.msa.banking.personal.domain.repository.PersonalHistoryRepositoryCustom;
+import feign.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface PersonalHistoryJpaRepository extends JpaRepository<PersonalHistory, Long>, PersonalHistoryRepositoryCustom {
 
@@ -15,4 +20,11 @@ public interface PersonalHistoryJpaRepository extends JpaRepository<PersonalHist
     Optional<PersonalHistory> findById(Long historyId);
 
     PersonalHistory save(PersonalHistory personalHistory);
+
+    @Query("SELECT ph FROM PersonalHistory ph WHERE ph.userId = :userId AND ph.transactionDate BETWEEN :startDate AND :endDate")
+    List<PersonalHistory> findPersonalHistoryByDateRange(
+            @Param("userId") UUID userId,
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate);
+
 }
