4c43397705263188f05f7dd38379df4f123a71d2
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-10-13 11:46:01 +0900

    refactor : 매출성과 스케줄러 기능 수정

--- com.banking.account/src/main/java/com/msa/banking/account/infrastructure/scheduler/PerformanceScheduler.java
+++ com.banking.account/src/main/java/com/msa/banking/account/infrastructure/scheduler/PerformanceScheduler.java
@@ -37,22 +37,34 @@ public class PerformanceScheduler {
 
         // 슬랙 ID 리스트
         List<String> slackIds = deserialize.getSlackIds();
+        for (int i = 0; i < slackIds.size(); i++) {
+            System.out.println("master slackId " + i + "= " + slackIds.get(i));
+        }
 
         // 대출 가입 건수
         int loanCount = deserialize.getLoanCount();
+        System.out.println("대출 가입 건수 = " + loanCount);
 
         // 계좌 ID 리스트
         List<UUID> accountIds = deserialize.getAccountIds();
+        for (UUID accountId : accountIds) {
+            System.out.println("대출 계좌 id = " + accountId);
+        }
 
-        // 전월의 시작일
-        YearMonth lastMonth = YearMonth.now().minusMonths(1);
-        LocalDateTime startDateTime = lastMonth.atDay(1).atStartOfDay();
+//        // 전월의 시작일
+//        YearMonth lastMonth = YearMonth.now().minusMonths(1);
+//        LocalDateTime startDateTime = lastMonth.atDay(1).atStartOfDay();
+//
+//        // 전월의 마지막일
+//        LocalDateTime endDateTime = lastMonth.atEndOfMonth().atTime(23, 59, 59);
 
-        // 전월의 마지막일
-        LocalDateTime endDateTime = lastMonth.atEndOfMonth().atTime(23, 59, 59);
+        // 현재 월의 YearMonth 객체 생성
+        YearMonth currentMonth = YearMonth.now();
+        // 현재 월의 시작일 (1일 00:00:00)
+        LocalDateTime startDateTime = currentMonth.atDay(1).atStartOfDay();
 
-        // 계좌 리스트 조회
-//        List<String> findAccountNumber = accountRepository.findByAccountIdInAndCreatedAtBetween(accountIds, startDateTime, endDateTime);
+        // 현재 월의 마지막일 (23:59:59)
+        LocalDateTime endDateTime = currentMonth.atEndOfMonth().atTime(23, 59, 59);
 
         // 대출 토탈 거래 금액
         BigDecimal totalAmount = transactionsRepository.findTotalAmount(accountIds, TransactionType.LOAN_REPAYMENT, startDateTime, endDateTime);
