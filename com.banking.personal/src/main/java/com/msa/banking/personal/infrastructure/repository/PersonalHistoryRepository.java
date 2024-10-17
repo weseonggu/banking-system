91f2446e3955b8c75f44dfde7bb1433bdf4d6eb5
beomsu1 <beumsu1221@gmail.com>
2024-10-17 20:17:59 +0900

    feat: 가장 많은 금액을 소비한 카테고리, 금액 조회 API 생성

--- com.banking.personal/src/main/java/com/msa/banking/personal/infrastructure/repository/PersonalHistoryRepository.java
+++ com.banking.personal/src/main/java/com/msa/banking/personal/infrastructure/repository/PersonalHistoryRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -19,6 +20,19 @@ public interface PersonalHistoryRepository extends JpaRepository<PersonalHistory
 
     @Query("SELECT SUM(ph.amount) FROM PersonalHistory ph WHERE ph.userId = :userId AND ph.transactionDate BETWEEN :startDate AND :endDate")
     Optional<BigDecimal> findTotalAmountByDateRange(
+            @Param("userId") UUID userId,
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate);
+
+    @Query("SELECT c.name AS categoryName, SUM(ph.amount) AS totalSpent " +
+            "FROM PersonalHistory ph " +
+            "JOIN ph.category c " +  // 엔티티 관계에 따른 조인
+            "WHERE ph.userId = :userId " +
+            "AND ph.transactionDate BETWEEN :startDate AND :endDate " +
+            "AND ph.status <> 'UNCLASSIFIED' " +  // UNCLASSIFIED 상태 제외
+            "GROUP BY c.name " +
+            "ORDER BY totalSpent DESC")
+    List<Object[]> findMostSpentCategoryByUserIdAndDateRange(
             @Param("userId") UUID userId,
             @Param("startDate") LocalDateTime startDate,
             @Param("endDate") LocalDateTime endDate);
